6e922be35f2142bb6b8c433cc697c9c67b957466
apxxxxxxe <calcium629@gmail.com>
2022-04-02 16:01:36 +0900

    定期コミット

--- io/io.go
+++ io/io.go
@@ -2,15 +2,17 @@ package io
 
 import (
 	"bufio"
+	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 )
 
-func GetLines(path string) []string {
+func GetLines(path string) ([]string, error) {
 	pwd, _ := os.Getwd()
 	fp, err := os.Open(filepath.Join(pwd, path))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer fp.Close()
 
@@ -20,7 +22,47 @@ func GetLines(path string) []string {
 		lines = append(lines, scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
-		panic(err)
+		return nil, err
 	}
-	return lines
+	return lines, nil
+}
+
+func WriteLine(path string, line string) {
+	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	fmt.Fprintln(f, line)
+}
+
+func DeleteLine(path string, line string) error {
+	lines, err := GetLines(path)
+	if err != nil {
+		return err
+	}
+
+	os.Remove(path)
+
+	result := lines
+	for i, l := range lines {
+		if l != line {
+			WriteLine(path, l)
+		}
+	}
+
+	return nil
+}
+
+func removeDuplicate(arr []string) []string {
+	results := make([]string, 0, len(arr))
+	encountered := map[string]bool{}
+	for i := 0; i < len(arr); i++ {
+		if !encountered[arr[i]] {
+			encountered[arr[i]] = true
+			results = append(results, arr[i])
+		}
+	}
+	return results
 }
